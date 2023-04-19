e55a3ac5b099f8bc27ce6b1a65157b537f3c651e
Nikolay Elshin <nikolay19830223@gmail.com>
2023-04-19 21:16:00 +0800

    good

--- main.cpp
+++ main.cpp
@@ -3,31 +3,31 @@
 
 using namespace std;
 
-void Exchange(int a, int b);
+void Exchange(int* a, int* b);
 
 void main()
 {
 	string delimetr = "--------------------";
 	setlocale(LC_ALL, "");
 	int a = 2, b = 3;
-	cout << a << "\t" << b << endl;
-	cout << &a << "\t" << &b << endl;
+	cout << a << "\t \t \t" << b << endl;
+	cout << &a << "\t"  << &b << endl;
 	cout << delimetr << endl << delimetr << endl;
-	Exchange(a, b);
+	Exchange(&a, &b);
 	cout << delimetr << endl << delimetr << endl;
-	cout << a << "\t" << b << endl;
+	cout << a << "\t \t \t" << b << endl;
 	cout << &a << "\t" << &b << endl;
 
 }
 
-void Exchange(int a, int b)
+void Exchange(int* a, int* b)
 {
+	cout << *a << "\t \t \t" << *b << endl;
 	cout << a << "\t" << b << endl;
-	cout << &a << "\t" << &b << endl;
-	int buffer = a;
-	a = b;
-	b = buffer;
+	int buffer = *a;
+	*a = *b;
+	*b = buffer;
+	cout << *a << "\t \t \t" << *b << endl;
 	cout << a << "\t" << b << endl;
-	cout << &a << "\t" << &b << endl;
 }
 
